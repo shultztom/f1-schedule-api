c258fbffebce0995354c3e5fa73811bdc35c17c1
shultztom <shultzthomask@sau.edu>
2023-04-03 21:54:25 -0500

    added helmet and cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import express, { Express } from 'express';
 import bodyParser from "body-parser";
 import indexRouter from "./routes/index";
 import calendar from "./routes/calendar";
+import helmet from "helmet";
+import cors from "cors";
 
 // Express Config
 const app: Express = express();
 const port = 8080;
+app.use(helmet());
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/", indexRouter);
 app.use("/calendar", calendar);
